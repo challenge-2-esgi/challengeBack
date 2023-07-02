410d185329e1291d353a371dc67945756cc6ac77
izemaghilas <saidtala23@gmail.com>
2023-07-02 23:45:48 +0200

    make candidate api a microservice

---
+++ services/recruiter-api/src/config/tcpOptions.ts
@@ -0,0 +1,34 @@
+import { ConfigService } from '@nestjs/config';
+import { ClientProvider, TcpOptions, Transport } from '@nestjs/microservices';
+
+enum Services {
+  AUTH_SERVICE = 'AUTH_SERVICE',
+  CANDIDATE_SERVICE = 'CANDIDATE_SERVICE',
+}
+
+export default (configService: ConfigService) =>
+  ({
+    transport: Transport.TCP,
+    options: {
+      host: configService.get('MICROSERVICE_HOST'),
+      port: configService.get('MICROSERVICE_PORT'),
+    },
+  } as TcpOptions);
+
+const authService = (configService: ConfigService): ClientProvider => ({
+  transport: Transport.TCP,
+  options: {
+    host: configService.get('AUTH_SERVICE_HOST'),
+    port: configService.get('AUTH_SERVICE_PORT'),
+  },
+});
+
+const candidateService = (configService: ConfigService): ClientProvider => ({
+  transport: Transport.TCP,
+  options: {
+    host: configService.get('CANDIDATE_SERVICE_HOST'),
+    port: configService.get('CANDIDATE_SERVICE_PORT'),
+  },
+});
+
+export { Services, authService, candidateService };
--- services/recruiter-api/src/main.ts
+++ services/recruiter-api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import tcpOptions from './config/tcpOptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.connectMicroservice(tcpOptions(configService));
+  app.startAllMicroservices();
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
