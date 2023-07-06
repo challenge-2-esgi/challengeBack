9e8333d5398f2921a02e70a9bcce118b3c87d6df
Kagistan EESHVARAN <eeshvarankagistan@gmail.com>
2023-07-06 11:51:47 +0200

    elastic search index job offers, search job offers, docker compose

--- services/recruiter-api/src/job-offer/job-offer.module.ts
+++ services/recruiter-api/src/job-offer/job-offer.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { JobOfferService } from './job-offer.service';
 import { JobOfferController } from './job-offer.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { JobOfferSearchService } from './job-offer-search.service';
 
 @Module({
+  imports: [
+    ConfigModule,
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        node: configService.get('ELASTICSEARCH_NODE'),
+        auth: {
+          username: configService.get('ELASTICSEARCH_USERNAME'),
+          password: configService.get('ELASTICSEARCH_PASSWORD'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [ElasticsearchModule],
   controllers: [JobOfferController],
-  providers: [JobOfferService, PrismaService],
+  providers: [JobOfferService, PrismaService, JobOfferSearchService],
 })
 export class JobOfferModule {}
